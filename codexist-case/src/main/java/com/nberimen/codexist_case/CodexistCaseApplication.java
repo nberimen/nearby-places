7e3eb9e66fc907527de8b325898c3e62e9f86505
nberimen <berimen66@gmail.com>
2024-07-31 22:46:22 +0300

    create backend project

---
+++ codexist-case/src/main/java/com/nberimen/codexist_case/CodexistCaseApplication.java
@@ -0,0 +1,13 @@
+package com.nberimen.codexist_case;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodexistCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodexistCaseApplication.class, args);
+	}
+
+}
